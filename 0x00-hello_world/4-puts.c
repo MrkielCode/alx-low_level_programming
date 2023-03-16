4914d53c3c2b4af2a6cb19efd3c8a0545a993b76
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-16 12:06:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *c Program to output words
+ *main - entry point
+ *return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
