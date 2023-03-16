79800873820ef908520396f9f719b37e9eb6bcca
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-16 12:10:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- *main - entry point
- *return: always 0 (success)
+ * main - Entry point
+ *
+ * Descriptiom: 'program for puts'
+ * Return: always 0 (success)
  */
 int main(void)
 {
