f7853b69a518cce9ae46c5c226a57612081ba2b3
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-19 13:19:25 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Description: 'program to print base16'
+ *
+ * Return Always (0) success
+ */
+
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n <= 9; n++){
+		putchar(n + '0');
+	}
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+
+}
