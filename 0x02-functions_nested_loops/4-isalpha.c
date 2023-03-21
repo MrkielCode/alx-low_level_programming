4b0ed0128d519affaf8832fb4010c42d6fdb745e
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-21 11:26:05 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+/**
+ * _isalpha - Function to check alphabet
+ *
+ * @c: Parameters to be printed
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
