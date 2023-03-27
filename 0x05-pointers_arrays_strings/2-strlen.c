5097e6bb4bee4e963fbbe4eee51e6c201e32610f
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-27 08:25:09 -0700

    checking string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - function to check length of the string
+ *
+ * @s: parameter to be printed
+ *
+ * Return: Always void
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
