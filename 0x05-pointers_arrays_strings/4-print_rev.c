b86afc71ebddc26d8d87692e669f2d588c3454bf
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-27 09:52:01 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - functions to reverse string
+ *
+ * @s: parameter to be printed
+ *
+ * Return: Always void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	for( i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
