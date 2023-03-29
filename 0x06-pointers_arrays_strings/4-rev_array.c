d13632bb7ca864d564038e5f29bf1e4f508d378a
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-03-29 14:23:13 -0700

    reverse integer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+ * print_array - function to reverse array
+ * @a: array parameter
+ * @n: index
+ * Return: Always TRUE
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
