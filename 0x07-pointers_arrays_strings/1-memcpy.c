cce2aa68cd6c1f20f8ec0e87a8218b230f88fbcb
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-03 07:58:10 -0700

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,19 @@
-#include "main.h"
-/**
- * _memcpy - this function copy a memory in another variable
- * @dest:  destination parameter of the string
- * @src: source parameter of the string
- * @n: length of the string
+/*
+ * _memcpy - functions to copy memory
+ * @dest: parameter destination
+ * @src: parameter source
  * Return: Always dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int k;
-	char *d = (char *) dest;
-	char *s = (char *) src;
+	char *d = (char *)dest;
+	char *s = (char *)src;
+	unsigned int j;
 
-	for (k = 0; k < n; k++)
+	for (j = 0; j < n; j++)
 	{
-		d[k] = s[k];
+		d[j] = s[j];
 	}
 	return (d);
 }
