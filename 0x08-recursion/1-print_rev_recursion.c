9e8ac9fda1c716991d8005613e62bcca2853d594
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-05 09:03:11 -0700

    recursive reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void _print_rev_recursion - recursive function to reverse a string
+ * _print_rev_recursion - recursive function to reverse a string
  * @s: character to be reverse
  * Return: Always s;
  */
