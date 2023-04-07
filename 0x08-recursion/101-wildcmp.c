cbb01b68c022df2304c8ec8250937cb29b5e9240
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-06 17:52:39 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+/**
+ * wildcmp - string compare
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Always 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 == '*' && *s2 != '\0')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
+
