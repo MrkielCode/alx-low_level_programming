9f7d57ae03c766286b86468496d464bf48724b4c
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-08 02:32:20 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	int a = 0;
-	int b = 0;
-
 	if (argc == 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
 		printf("%d\n", a * b);
 	}
 	else
