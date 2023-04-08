63b7e7d98a3f5f0a650f99e92c40aaa4216db052
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-08 14:36:57 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  * @argc: arguiment count
@@ -8,26 +9,44 @@
  * Return: Always 0.
  */
 
+int checkDigit(char *s)
+{
+	unsigned int count = 0;
+
+	while(count < strlen(s))
+	{
+		if (s[count] < 48 || s[count] > 57)
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
 int main(int argc, char *argv[])
 {
 	int sum = 0, count;
 
-	if (argc > 1)
+	/*if (argc > 1)*/
 	{
 		for (count = 1; count < argc; count++)
 		{
-			if (*argv[count] < 48 || *argv[count] > 57)
+			if (checkDigit(argv[count]))
+			{
+				sum += atoi(argv[count]);
+			}
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
-			sum += atoi(argv[count]);		
+
 		}
 		printf("%d\n", sum);
-	}
-	else
+	} 
+	/*else 
 	{
 		printf("0\n");
-	}
+	}*/
 	return (0);
 }
