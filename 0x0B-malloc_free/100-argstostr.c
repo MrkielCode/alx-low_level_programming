2dfe91566393254508aa374174ab021615699a4d
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-12 10:00:11 -0700

    argstostar

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - function to concatenate arguments
+ * @ac: argument constant
+ * @av: argument varibale
+ * Return: Pointer or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int counter1, counter2, len, j;
+	char *conStr;
+
+	len = j = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (counter1 = 0; counter1 < ac; counter1++)
+		len += strlen(av[counter1]) + 1;
+
+	conStr = malloc(sizeof(char) * len);
+	if (conStr == NULL)
+		return (NULL);
+
+	counter1 = 0;
+	while (counter1 < ac)
+	{
+		for (counter2 = 0; av[counter1][counter2] != '\0'; counter2++)
+			conStr[j++] = av[counter1][counter2];
+		conStr[j++] = '\n';
+		counter1++;
+	}
+
+	conStr[j] = '\0';
+	return (conStr);
+}
