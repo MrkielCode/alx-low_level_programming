96ebdde486c950045ebe063bd2e0e58dffb486ef
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-13 08:19:11 -0700

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * string_nconcat - concatenates two strings up to n bytes
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the maximum number of bytes to concatenate from s2
+ *
+ * Return: a newly allocated string containing s1 followed by the first n
+ * bytes of s2, or NULL if the allocation fails
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
     char *result;
     unsigned int s1_len, s2_len, result_len;
 
-    // Handle NULL inputs
-    if (s1 == NULL) {
+    if (s1 == NULL)
         s1 = "";
-    }
-    if (s2 == NULL) {
+    if (s2 == NULL)
         s2 = "";
-    }
 
     s1_len = strlen(s1);
     s2_len = strlen(s2);
 
-    // Adjust n if necessary
-    if (n > s2_len) {
+    if (n > s2_len)
         n = s2_len;
-    }
 
-    // Compute length of result string
     result_len = s1_len + n;
 
-    // Allocate memory for result string
-    result = (char *) malloc(result_len + 1);
+    result = malloc(result_len + 1);
+    if (result == NULL)
+        return (NULL);
 
-    if (result == NULL) {
-        return NULL;
-    }
-
-    // Copy s1 into result
     memcpy(result, s1, s1_len);
-
-    // Copy up to n bytes of s2 into result
     memcpy(result + s1_len, s2, n);
-
-    // Add null terminator
     result[result_len] = '\0';
 
-    return result;
+    return (result);
 }
+
