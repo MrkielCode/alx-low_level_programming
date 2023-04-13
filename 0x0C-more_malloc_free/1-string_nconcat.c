1cc6ab0c4c6287bddf1c7bd94f9e3341a01f6a2d
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-13 08:35:48 -0700

    string n concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,35 +7,37 @@
  * @s2: the second string
  * @n: the maximum number of bytes to concatenate from s2
  *
- * Return: a newly allocated string containing s1 followed by the first n
- * bytes of s2, or NULL if the allocation fails
+ * Return: NULL
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int s1_len, s2_len, result_len;
+	char *result;
+	unsigned int s1_len, s2_len, result_len;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
+	if (s2 == NULL)
+		s2 = "";
 
-    if (n > s2_len)
-        n = s2_len;
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
-    result_len = s1_len + n;
+	if (n > s2_len)
+		n = s2_len;
 
-    result = malloc(result_len + 1);
-    if (result == NULL)
-        return (NULL);
+	result_len = s1_len + n;
 
-    memcpy(result, s1, s1_len);
-    memcpy(result + s1_len, s2, n);
-    result[result_len] = '\0';
+	result = malloc(result_len + 1);
 
-    return (result);
+	if (result == NULL)
+		return (NULL);
+
+	memcpy(result, s1, s1_len);
+	memcpy(result + s1_len, s2, n);
+	result[result_len] = '\0';
+
+	return (result);
 }
 
