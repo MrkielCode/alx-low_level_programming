cba6f227d4b7dc4e47c5943eb00fc3974334fd6b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-17 14:30:34 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: first memeber
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
