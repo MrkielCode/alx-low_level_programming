3403c968d3cd9019fbb383464c6c7bfd3c2ecece
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-18 06:10:03 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,14 +23,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(l1));
+	dog->name = malloc(sizeof(char *) * l1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(l2));
+	dog->owner = malloc(sizeof(char *) * l2);
 	if (dog->owner == NULL)
 	{
 		free(dog);
