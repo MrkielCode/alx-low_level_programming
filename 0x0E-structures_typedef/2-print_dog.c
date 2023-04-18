8739092687994c4465abc9476de7c464f1471dee
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-18 04:53:04 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,9 +8,13 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name == NULL)
 		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 	printf("%s\n", d->name);
 	printf("%f\n", d->age);
 	printf("%s\n", d->owner);
