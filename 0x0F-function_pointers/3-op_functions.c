f0530d85dfcca4ff56bfe9018859f53b66a32b31
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-19 16:03:28 -0700

    simple calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - to add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a & b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - to sub two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sub of a & b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - to mul two numbers
+ * @a: first number
+ * @b: second number
+ * Return: mul of a & b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - to div two numbers
+ * @a: first number
+ * @b: second number
+ * Return: div of a & b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - to mod two numbers
+ * @a: first number
+ * @b: second number
+ * Return: mod of a & b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
