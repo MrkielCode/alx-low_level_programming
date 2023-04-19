d75ada60f721d9d03f8c18c447f1c93a121e512c
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-04-19 07:09:07 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - to print name
  * @name: name to be printer
@@ -6,5 +7,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
