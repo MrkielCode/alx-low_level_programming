ac6c8bad0232bcdc7291b61329df20ea90731fd5
MrkielCode <ikechukwi.ezekiel@gmail.com>
2023-04-29 17:02:53 -0700

    add node at beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - add new node at the beginning of
+ * the linked list
+ * @head: head of the linked list
+ * @n: node to be added
+ * Return: NULL or New Node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+
+}
