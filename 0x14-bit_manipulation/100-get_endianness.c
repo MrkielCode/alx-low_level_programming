e585b43fbaa9752eba6abceb6132370f0b207f3f
MrkielCode <ikechukwi.ezekiel@gmail.com>
2023-05-04 16:20:19 -0700

    get machine size

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - check machine size
+ * Return: 0 or  1
+ */
+int get_endianness(void)
+{
+	unsigned int j = 1;
+	char *c = (char *) &j;
+	return (*c);
+}
