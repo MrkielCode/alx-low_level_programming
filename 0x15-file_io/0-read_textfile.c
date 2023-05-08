69acc8a435678d1cc3a9418616094205151b4518
MrkielCode <ikechukwi.ezekiel@gmail.com>
2023-05-08 10:14:11 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,6 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t rd;
 	ssize_t *buffer;
 	ssize_t file;
+	ssize_t wr;
+
+	if (filename == NULL)
+		return (0);
 
 	file = open(filename, O_RDONLY);
 
@@ -21,8 +25,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-
 	rd = read(file, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
 
-	return (write(STDOUT_FILENO, buffer, rd));
+	if (wr == -1)
+		return (0);
+
+	return (wr);
 }
