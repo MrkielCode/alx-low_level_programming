30770a7953c9d27a76b86a1e204001c4bcde7706
MrkielCode <ikechukwi.ezekiel@gmail.com>
2023-05-08 10:18:26 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -30,6 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (wr == -1)
 		return (0);
-
+	free(buffer);
+	close(file);
 	return (wr);
 }
