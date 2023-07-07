98d96a057818639a6073885c1cb38b928dcfb76c
mrkielcode <ikechukwu.ezekiel@gmail.com>
2023-07-07 20:01:06 +0100

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * clear_bit - set a bit
+ * @n: parameter to be set
+ * @index: index of the number
+ * Return: 1 or -1;
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mk;
+	int i = -1;
+	unsigned int maximum_index = 63;
+
+
+	if (index > maximum_index)
+		return (i);
+
+	mk = ~(1UL << index);
+
+	*n &= mk;
+
+	return (1);
+}
