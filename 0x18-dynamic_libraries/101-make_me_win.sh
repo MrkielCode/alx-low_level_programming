06f67ab0e9f8e1bf07ce8ff38e425e260412a075
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-21 21:28:10 +0100

    hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MrkielCode/alx-low_level_programming/raw/main/0x18-dynamic_libraries/myrand.so
-export LD_PRELOAD=/tmp/myrand.so
+wget -P /tmp https://github.com/MrkielCode/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libtest.so 
+export LD_PRELOAD=/tmp/libtest.so
