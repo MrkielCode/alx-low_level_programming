13d221b58a4ada91a889815ed8107f242766e18b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-21 21:22:12 +0100

    push

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp/ https://raw.github.com/MrkielCode/alx-low_level_programming/main/0x18-dynamic_libraries/myrand.so
+wget -P /tmp https://github.com/MrkielCode/alx-low_level_programming/raw/main/0x18-dynamic_libraries/myrand.so
 export LD_PRELOAD=/tmp/myrand.so
