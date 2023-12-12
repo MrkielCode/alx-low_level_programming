e67a54072db465812c8dfe22263c35be057a8061
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-12 16:44:52 +0100

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,14 +10,14 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0;
 	size_t right = size - 1;
-	size_t i;
+	size_t i, mid;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	while (left <= right)
 	{
-		size_t mid = left + (right - left) / 2;
+		mid = left + (right - left) / 2;
 
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
